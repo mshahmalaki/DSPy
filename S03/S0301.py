a3e284de3800f7b644101d61d62412777c82ab27
mshahmalaki <mshahmalaki@gmail.com>
2020-11-27 22:10:46 +0330

    Assignment 01 from Session 03

---
+++ S03/S0301.py
@@ -0,0 +1,10 @@
+lst = [3, 2, 4, 5, 6, 7, 1, 0, 9, 8, 7]
+lst_even = []
+lst_odd = []
+for i in lst:
+    if i % 2 == 0:
+        lst_even.append(i)
+    else:
+        lst_odd.append(i)
+print('List of even numbers :', lst_even)
+print('Count of odd numbers ', len(lst_odd))
