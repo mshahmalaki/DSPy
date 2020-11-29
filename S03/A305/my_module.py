ea0bd5a0760a2fa142c53f10624aa78111411347
mshahmalaki <mshahmalaki@gmail.com>
2020-11-29 23:27:16 +0330

    change if condition - Thanks K.A

--- S03/A305/my_module.py
+++ S03/A305/my_module.py
@@ -1,7 +1,7 @@
 def my_func(lst):
     string_list = []
     for i in lst:
-        if type(i).__name__ == 'str':
+        if type(i) == str:
             string_list.append(i)
     print(string_list)
     print(len(string_list))
