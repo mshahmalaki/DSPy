d4e70bcb74050af77858169459158eaca974b4c0
mshahmalaki <mshahmalaki@gmail.com>
2020-11-29 17:30:52 +0330

    fix my_module

--- S03/A305/my_module.py
+++ S03/A305/my_module.py
@@ -1,3 +1,7 @@
 def my_func(lst):
-    print(lst)
-    print(len(lst))
+    string_list = []
+    for i in lst:
+        if type(i).__name__ == 'str':
+            string_list.append(i)
+    print(string_list)
+    print(len(string_list))
