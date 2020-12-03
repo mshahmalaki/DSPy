bf09ed1548a17e91ebe1ee3db5d8ffe030f5df94
mshahmalaki <mshahmalaki@gmail.com>
2020-12-03 16:52:17 +0330

    change bar to barh

--- s04/a402/a402.py
+++ s04/a402/a402.py
@@ -6,7 +6,6 @@ city_list = np.array(['Tehran', 'Karaj'])
 pop_1390 = np.array([12183391, 2412513])
 pop_1395 = np.array([13267637, 2712400])
 
-width = 0.3
-plt.bar(city_list, pop_1395, color='g', width=width)
-plt.bar(city_list, pop_1390, color='y', width=width)
+plt.barh(city_list, pop_1395, color='g')
+plt.barh(city_list, pop_1390, color='y')
 plt.show()
