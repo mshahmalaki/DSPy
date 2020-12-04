c0583d7bd5615e142d58aa53fbde11f02549e4bf
mshahmalaki <mshahmalaki@gmail.com>
2020-12-04 20:17:57 +0330

    add compare_array function

--- s04/a406/a406.py
+++ s04/a406/a406.py
@@ -1,19 +1,23 @@
 import numpy as np
 
 
+def compare_array(array1, array2):
+    if array1.ndim > array2.ndim:
+        print('Number of dimensions of the first array is greater than \n'
+              'Number of dimensions of the second array')
+    elif array2.ndim > array1.ndim:
+        print('Number of dimensions of the second array is greater than \n'
+              'Number of dimensions of the first array')
+    else:
+        print('Number of dimensions of the first array is equal \n'
+              'Number of dimensions of the second array')
+
+
 np_array_1 = np.array([[1, 2, 3],
                        [4, 5, 6],
                        [7, 8, 9]])
 np_array_2 = np.array([[[1, 2], [3, 4]],
                        [[5, 6], [7, 8]]])
-print(np_array_1.shape)
-print(np_array_2.shape)
-if np_array_1.ndim > np_array_2.ndim:
-    print('Number of dimensions of the first array is greater than \n'
-          'Number of dimensions of the second array')
-elif np_array_2.ndim > np_array_1.ndim:
-    print('Number of dimensions of the second array is greater than \n'
-          'Number of dimensions of the first array')
-else:
-    print('Number of dimensions of the first array is equal \n'
-          'Number of dimensions of the second array')
+
+compare_array(np_array_1, np_array_2)
+
