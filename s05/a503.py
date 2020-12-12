c161a2b758fca552a24b9ca3fe7fb8cc3b3c4525
Mohammad Shahmaleki <mshahmalaki@gmail.com>
2020-12-12 04:10:23 +0330

    Assignment 03 from Session 05

---
+++ s05/a503.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+
+excel = pd.ExcelFile(r'./customerfeedback.xlsx')
+customer_sheet = pd.read_excel(excel, sheet_name='Customer Table')
+geography_sheet = pd.read_excel(excel, sheet_name='Geography')
+
+# Left join
+new_customer_sheet_01 = customer_sheet.merge(geography_sheet, how='left',
+                                          left_on='Geography ID',
+                                          right_on='Geography ID')
+print(new_customer_sheet_01.head(10))
+
+# Fuzzy matching
+new_customer_sheet_02 = customer_sheet['Geography ID'].replace(geography_sheet.set_index('Geography ID').Continent.to_dict())
+print(new_customer_sheet_02.head(10))
