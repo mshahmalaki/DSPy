989e09a26b9a76ca738bdce67d23667ce29805e2
Mohammad Shahmaleki <mshahmalaki@gmail.com>
2020-12-12 03:33:14 +0330

    Update dataframe name

--- s05/a501.py
+++ s05/a501.py
@@ -9,5 +9,5 @@ sum_original_score = df['Original Score'].sum()
 count_original_score = df['Original Score'].count()
 avg_original_score = sum_original_score / count_original_score
 
-new_df = df['Original Score'].replace(np.nan, avg_original_score)
-print(new_df.head())
+new_original_score = df['Original Score'].replace(np.nan, avg_original_score)
+print(new_original_score.head())
