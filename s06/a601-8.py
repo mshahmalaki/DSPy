dfc1388058b7b05c405f6835c0ac6efda70f9936
Mohammad Shahmaleki <mshahmalaki@gmail.com>
2020-12-17 14:45:08 +0330

    Assignment 04 from Session 06

--- s06/a601-8.py
+++ s06/a601-8.py
@@ -1,11 +1,14 @@
 import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
 
 
-pbc = pd.read_csv(r'./pedestrian-and-bicyclist-counts.csv')
+csv = pd.read_csv(r'./pedestrian-and-bicyclist-counts.csv')
+pbc = pd.DataFrame(csv)
 print('---------------Answer of question 01---------------')
 pbc01 = pbc.copy()
 pbc01['People'] = pbc01['Pedestrians'] + pbc01['Cyclists']
-print(pbc.head())
+print(pbc01.head())
 print('---------------Answer of question 02---------------')
 pbc02 = pbc.copy()
 pbc02 = pbc02[(pbc02['Cyclists'] > pbc02['Pedestrians'])]
@@ -15,3 +18,6 @@ print('---------------Answer of question 03---------------')
 pbc03 = pbc.copy()
 print('Average number of cyclists and pedestrians observed per day:')
 print(pbc03.mean())
+print('---------------Answer of question 04---------------')
+sns.kdeplot(pbc['Pedestrians'])
+plt.show()
