8a4216e8db29e593e6791ed2f4b9ee9a52c94379
Mohammad Shahmaleki <mshahmalaki@gmail.com>
2020-12-17 14:03:21 +0330

    Assignment 02 from Session 06

--- s06/a601-8.py
+++ s06/a601-8.py
@@ -5,3 +5,8 @@ pbc = pd.read_csv(r'./pedestrian-and-bicyclist-counts.csv')
 pbc['People'] = pbc['Pedestrians'] + pbc['Cyclists']
 print('---------------Answer of question 01---------------')
 print(pbc.head())
+print('---------------Answer of question 02---------------')
+pbc02 = pbc.copy()
+pbc02 = pbc02[(pbc02['Cyclists'] > pbc02['Pedestrians'])]
+count_days = pbc02['Date'].count()
+print('The number of days that has more cyclists than pedestrians :', count_days, 'day(s)')
