4b7630a24477bfed90cc08f6e95f85d6af7049c2
Mohammad Shahmaleki <mshahmalaki@gmail.com>
2021-01-29 13:16:14 +0330

    plot normal dist for all columns

--- s11/a1101.py
+++ s11/a1101.py
@@ -1,7 +1,18 @@
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
 housing = pd.read_csv(r'housing.csv')
 housing.dropna(inplace=True)
-print(housing.count())
+# print(housing.count())
 
+# Normaliztion
+# MinMax method
+scaler1 = MinMaxScaler()
+minmax_method = pd.DataFrame(scaler1.fit_transform(housing.iloc[:, :-1]))
+print(minmax_method.head())
+
+sns.distplot(minmax_method.iloc[:, :-1])
+plt.show()
