c598fc908d7575d7a73fcc6e978de81727a2d2a6
Mohammad Shahmaleki <mshahmalaki@gmail.com>
2021-01-29 16:14:06 +0330

    plot normal dist for all columns with zscore method

--- s11/a1101.py
+++ s11/a1101.py
@@ -1,18 +1,28 @@
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
 
 
 housing = pd.read_csv(r'housing.csv')
 housing.dropna(inplace=True)
 # print(housing.count())
 
-# Normaliztion
+# Normalization
 # MinMax method
 scaler1 = MinMaxScaler()
-minmax_method = pd.DataFrame(scaler1.fit_transform(housing.iloc[:, :-1]))
-print(minmax_method.head())
+minmax_method = pd.DataFrame.copy(housing)
+minmax_method.iloc[:, :-1] = scaler1.fit_transform(housing.iloc[:, :-1])
+print(minmax_method.iloc[:, :-1].head)
+# sns.distplot(minmax_method.iloc[:, :-1])
 
-sns.distplot(minmax_method.iloc[:, :-1])
+
+# Normalization
+# ZScore method
+scaler2 = StandardScaler()
+zscore_method = pd.DataFrame.copy(housing)
+zscore_method.iloc[:, :-1] = scaler2.fit_transform((housing.iloc[:, :-1]))
+print(zscore_method.iloc[:, :-1].head)
+sns.distplot(zscore_method.iloc[:, :-1])
 plt.show()
